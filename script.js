ea0a0e263622aa5e1a5ad756ec2a2488589cacd3
Brian Rodriguez <Rodriguezantoniobrian@gmail.com>
2023-01-31 19:11:12 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+function play() {
+    var mainVideo = document.querySelector("#video");
+    mainVideo.play();
+}
+
+function dontPlay() {
+    var mainVideo = document.querySelector("#video");
+    mainVideo.pause();
+}
+
+
